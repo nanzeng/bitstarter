b72895a864675f3e0bf67d84825c9f937c6b36a0
Nan Zeng <zengnan@gmail.com>
2013-07-11 13:50:18 +0800

    modified web.js to read content from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync("index.html");
+var message = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
